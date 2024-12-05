5a29283bb38885ba74f778504c9f54035db61e83
May <abeillee@bk.ru>
2024-12-06 02:02:48 +0300

    feat: add solution for repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  const separator = options.separator === undefined ? '+' : options.separator;
+  const additionSeparator = options.additionSeparator === undefined ? '|' : options.additionSeparator;
+  const repeatTimes = options.repeatTimes || 1;
+  const additionRepeatTimes = options.additionRepeatTimes || 1;
+  const str1 = typeof str === 'string' ? str : String(str);
+  const addition1 = options.addition === undefined ? '' : typeof addition === 'string' ? options.addition : String(options.addition); // если сущ-ет задаю значение, если не строка, делаю строкой, если не сущ-ет = пустая строка
+
+  let innerStr = '';
+  let outerStr = '';
+
+  for (let i = 0; i < additionRepeatTimes; i++) {   //создаю внутр. стр.
+    innerStr += addition1;
+    if (i < additionRepeatTimes - 1) {
+      innerStr += additionSeparator;
+    }
+
+  }
+  for (let i = 0; i < repeatTimes; i++) {         //создаю внешн. стр.
+    outerStr += str1 + innerStr;                  //добавляю поочередно внутр. стр.
+    if (i < repeatTimes - 1) {
+      outerStr += separator;
+    }
+  }
+  return outerStr;
 }
 
 module.exports = {
